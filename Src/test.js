452cac7485148597c0c97a3e3497a054163e4f4b
FernandoJesuss <fernando_ffernando@hotmail.com>
2024-07-10 23:15:42 -0300

    Fix responsive

---
+++ Src/test.js
@@ -0,0 +1,18 @@
+// document.querySelector('.menu-toggle input').addEventListener('click', function() {
+    // document.querySelector('.menu').classList.toggle('open');
+//   });
+  
+
+
+
+
+
+
+
+
+
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    menu.style.display = (menu.style.display === 'block' ? 'none' : 'block');
+  }
+  
